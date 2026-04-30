cfb1a76a43c0daa76665aa12ec4444fafb97880c
dealexis <flytzitv@gmail.com>
2026-04-30 13:01:31 +0200

    feat: add dev startup script for automated workspace launch

---
+++ start.ps1
@@ -0,0 +1,2 @@
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$PSScriptRoot\server'; npm run dev"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd '$PSScriptRoot\client'; npm run dev"
