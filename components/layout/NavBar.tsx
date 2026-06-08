f213208bdc801222daf004182842bab6b4579ef4
dealexis <flytzitv@gmail.com>
2026-06-08 23:02:18 +0200

    feat: refactor NavBar pour séparer les éléments de navigation gauche et droite

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -2,14 +2,26 @@
 
 import { NavBarItems } from "@/lib/navbar";
 import { NavBarProps } from "@/types";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+  const leftItems = NavBarItems.slice(0, 2);
+  const rightItems = NavBarItems.slice(2);
+
   return (
     <div className="fixed max-w-[430px] mx-auto bottom-0 left-0 right-0 p-4 bg-white border-t border-neutral-200">
       <div className="flex items-center justify-around">
-        {NavBarItems.map((item) => (
+        {leftItems.map((item) => (
+          <NavItem key={item.name} {...item} />
+        ))}
+
+        <button className="w-12 h-12 rounded-full bg-neutral-900 flex items-center justify-center shadow-lg">
+          <Plus size={20} className="text-white" />
+        </button>
+
+        {rightItems.map((item) => (
           <NavItem key={item.name} {...item} />
         ))}
       </div>
