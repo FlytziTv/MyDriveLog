6874f79cd1b19546e6786bcdb9fe63ad19b0bbb1
dealexis <flytzitv@gmail.com>
2026-06-09 14:19:54 +0200

    feat: ajouter le composant MiniStatsTabs pour afficher les statistiques de la voiture

---
+++ components/car/MiniStatsTabs.tsx
@@ -0,0 +1,32 @@
+import { MiniStatsCarProps, StatProfileProps } from "@/types";
+
+export default function MiniStatsTabs({ km, date, total }: MiniStatsCarProps) {
+  return (
+    <div className=" border-y border-neutral-200 p-4 flex flex-row items-center justify-around">
+      <Stats value={km || "-"} label="Kilomètres" />
+
+      <div className="w-px bg-neutral-200 self-stretch my-1" />
+
+      <Stats
+        value={new Date(date || "-").toLocaleDateString("fr-FR", {
+          day: "numeric",
+          month: "short",
+        })}
+        label="Dernière"
+      />
+
+      <div className="w-px bg-neutral-200 self-stretch my-1" />
+
+      <Stats value={total + " €" || "-"} label="Total" />
+    </div>
+  );
+}
+
+function Stats({ value, label }: StatProfileProps) {
+  return (
+    <div className="w-25 flex flex-col gap-0 items-center justify-center text-center">
+      <p className="text-lg font-bold text-neutral-900">{value}</p>
+      <p className="text-xs text-neutral-500">{label}</p>
+    </div>
+  );
+}
